647b27ba89b575398c6984c51638c814972b402d
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-12-23 00:02:00 -0500

    SubtractFive

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -23,3 +23,26 @@ describe('multiplybyten()', () => {
     expect(product).toBe(0);
   });
 });
+
+
+//subtractbyfive
+describe('subtractfive()', () => {
+  it('subtracts value by five', () => {
+    const expected = 6;
+    const actual = helpers.subtractFive(11);
+    expect(actual).toEqual(expected);
+  });
+
+  it('returns null if no value', () => {
+    const actual = helpers.subtractFive(undefined);
+    expect(actual).toBe(null);
+  });
+
+  it('error if a string', () => {
+    expect(() => {
+      helpers.subtractFive('123');
+    }).toThrow();
+    expect;
+  });
+
+});
