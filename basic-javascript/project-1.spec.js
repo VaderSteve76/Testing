e59da93eddf2fd753923fc0f8950b66d9a995c9d
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-12-22 23:50:37 -0500

    Multiplybyten spec

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,25 @@
 const helpers = require('./project-1');
-
 // start testing!
+test('run all tests', () => {});
+it('runs tests using it() while running test()', () => {});
+
+//multiplybyten
+describe('multiplybyten()', () => {
+  it('multiplies a number by ten', () => {
+    const expected = 100;
+    const actual = helpers.multiplyByTen(10);
+    expect(actual).toEqual(expected);
+  });
+
+  it('gives error if its a string', () => {
+    expect(() => {
+      helpers.multiplyByTen('123');
+    }).toThrow();
+    expect;
+  });
+
+  it('should return 0 if no value', () => {
+    const product = helpers.multiplyByTen();
+    expect(product).toBe(0);
+  });
+});
